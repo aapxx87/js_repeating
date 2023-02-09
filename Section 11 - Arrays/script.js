20c30a3156d0d3160e5435d4dc27443726c50f3a
aapxx87 <57785764+aapxx87@users.noreply.github.com>
2023-02-09 11:37:20 +0300

    update

--- Section 11 - Arrays/script.js
+++ Section 11 - Arrays/script.js
@@ -300,10 +300,41 @@ btnTransfer.addEventListener('click', function (e) {
         console.log('transfer valid');
 
     }
+})
 
 
+
+
+
+
+btnLoan.addEventListener('click', function (e) {
+
+    e.preventDefault()
+
+    const amount = Number(inputLoanAmount.value)
+
+    if (
+        amount > 0 &&
+        currentAccount.movements.some(mov => mov >= amount / 10)
+    ) {
+
+        // Add movement
+        currentAccount.movements.push(amount)
+
+        // Update UI
+        updateUI(currentAccount)
+    }
+
+    inputLoanAmount.value = ''
 
 })
+
+
+
+
+
+
+
 
 btnClose.addEventListener('click', function (e) {
 
